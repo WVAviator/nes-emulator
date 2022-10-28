0ec8d2d780a286001f921507876a3b31f5179b40
Wvaviator <wvaviator@gmail.com>
2022-10-28 14:52:55 -0500

    add functions for loading and adding

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,35 +1,46 @@
+pub mod opcodes;
+// pub mod stack; // does not work yet
+pub mod status;
 #[cfg(test)]
 mod test;
+
+use self::status::{Flag, ProcessorStatus};
+use std::collections::HashMap;
+
+const STACK_OFFSET: u16 = 0x0100;
+const STACK_RESET: u8 = 0xFD;
 
 pub struct CPU {
     pub register_a: u8, // accumulator - stores results of arithmetic and logic operations
     pub register_x: u8, // register x - temporarily stores values
-    pub status: u8,     // a register with 7 flags indicating cpu status after each instruction
-    pub program_counter: u16, // program counter holds two bytes, points to next instructions
+    pub register_y: u8, // register y - temporarily stores values
+    pub status: ProcessorStatus, // a register with 7 flags indicating cpu status after each instruction
+    pub program_counter: u16,    // program counter holds two bytes, points to next instructions
+    pub stack_pointer: u8,
     memory: [u8; 0xFFFF],
 }
 
-impl CPU {
-    pub fn new() -> Self {
-        CPU {
-            register_a: 0,
-            register_x: 0,
-            status: 0,
-            program_counter: 0,
-            memory: [0; 0xFFFF],
-        }
-    }
+#[derive(Debug)]
+#[allow(non_camel_case_types)]
+pub enum AddressingMode {
+    Immediate,
+    ZeroPage,
+    ZeroPage_X,
+    ZeroPage_Y,
+    Absolute,
+    Absolute_X,
+    Absolute_Y,
+    Indirect_X,
+    Indirect_Y,
+    NoneAddressing,
+}
 
-    fn mem_read(&self, addr: u16) -> u8 {
-        self.memory[addr as usize]
-    }
+trait Mem {
+    fn mem_read(&self, addr: u16) -> u8;
 
-    fn mem_write(&mut self, addr: u16, data: u8) {
-        self.memory[addr as usize] = data;
-    }
+    fn mem_write(&mut self, addr: u16, data: u8);
 
-    // NES uses little Endian addressing rather than big endian - must switch the bytes when reading/writing
-    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+    fn mem_read_u16(&self, pos: u16) -> u16 {
         let lo = self.mem_read(pos) as u16;
         let hi = self.mem_read(pos + 1) as u16;
         (hi << 8) | (lo as u16)
@@ -41,11 +52,84 @@ impl CPU {
         self.mem_write(pos, lo);
         self.mem_write(pos + 1, hi);
     }
+}
+
+impl Mem for CPU {
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+}
+
+impl CPU {
+    pub fn new() -> Self {
+        CPU {
+            register_a: 0,
+            register_x: 0,
+            register_y: 0,
+            status: ProcessorStatus::new(),
+            program_counter: 0,
+            stack_pointer: 0xFF,
+            memory: [0; 0xFFFF],
+        }
+    }
+
+    fn get_operand_address(&self, mode: &AddressingMode) -> u16 {
+        match mode {
+            AddressingMode::Immediate => self.program_counter,
+            AddressingMode::ZeroPage => self.mem_read(self.program_counter) as u16,
+            AddressingMode::ZeroPage_X => {
+                let pos = self.mem_read(self.program_counter);
+                let addr = pos.wrapping_add(self.register_x) as u16;
+                addr
+            }
+            AddressingMode::ZeroPage_Y => {
+                let pos = self.mem_read(self.program_counter);
+                let addr = pos.wrapping_add(self.register_y) as u16;
+                addr
+            }
+            AddressingMode::Absolute => self.mem_read_u16(self.program_counter),
+            AddressingMode::Absolute_X => {
+                let pos = self.mem_read_u16(self.program_counter);
+                let addr = pos.wrapping_add(self.register_x as u16);
+                addr
+            }
+            AddressingMode::Absolute_Y => {
+                let pos = self.mem_read_u16(self.program_counter);
+                let addr = pos.wrapping_add(self.register_y as u16);
+                addr
+            }
+            AddressingMode::Indirect_X => {
+                let base = self.mem_read(self.program_counter);
+
+                let ptr: u8 = (base as u8).wrapping_add(self.register_x);
+                let lo = self.mem_read(ptr as u16);
+                let hi = self.mem_read(ptr.wrapping_add(1) as u16);
+                (hi as u16) << 8 | (lo as u16)
+            }
+            AddressingMode::Indirect_Y => {
+                let base = self.mem_read(self.program_counter);
+
+                let lo = self.mem_read(base as u16);
+                let hi = self.mem_read((base as u8).wrapping_add(1) as u16);
+                let deref_base = (hi as u16) << 8 | (lo as u16);
+                let deref = deref_base.wrapping_add(self.register_y as u16);
+                deref
+            }
+            AddressingMode::NoneAddressing => {
+                panic!("mode {:?} is not supported", mode);
+            }
+        }
+    }
 
     pub fn reset(&mut self) {
         self.register_a = 0;
         self.register_x = 0;
-        self.status = 0;
+        self.register_y = 0;
+        self.stack_pointer = 0xFF;
+        self.status.reset();
 
         self.program_counter = self.mem_read_u16(0xFFFC);
     }
@@ -63,9 +147,32 @@ impl CPU {
 
     // LDA - Load Accumulator
     // Loads a byte of memory into the accumulator setting the zero and negative flags as appropriate
-    fn lda(&mut self, value: u8) {
+    fn lda(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+
         self.register_a = value;
         self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    // LDX - Load X Register
+    // Loads a byte of memory into the X register setting the zero and negative flags as appropriate.
+    fn ldx(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+
+        self.register_x = value;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    // LDY - Load Y Register
+    // Loads a byte of memory into the Y register setting the zero and negative flags as appropriate.
+    fn ldy(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+
+        self.register_y = value;
+        self.update_zero_and_negative_flags(self.register_y);
     }
 
     // TAX - Transfer Accumulator to X
@@ -78,58 +185,86 @@ impl CPU {
     // INX - Increment X Register
     // Adds one to the X register setting the zero and negative flags as appropriate
     fn inx(&mut self) {
-        if self.register_x == 0xff {
-            self.register_x = 0;
-        } else {
-            self.register_x += 1;
-        }
+        self.register_x = self.register_x.wrapping_add(1);
         self.update_zero_and_negative_flags(self.register_x);
     }
 
-    fn update_zero_and_negative_flags(&mut self, result: u8) {
-        // If A = 0, set the Zero Flag
-        if result == 0 {
-            // Only changes the Zero Flag to 1
-            self.status = self.status | 0b0000_0010;
-        } else {
-            // Only changes the Zero Flag to 0
-            self.status = self.status & 0b1111_1101;
-        }
+    fn adc(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(mode);
+        let value = self.mem_read(addr);
+        self.add_to_register_a(value);
+    }
 
-        // If bit 7 of result is set, set the Negative Flag
-        if result & 0b1000_0000 != 0 {
-            // Only changes the Negative Flag to 1
-            self.status = self.status | 0b1000_0000;
-        } else {
-            // Only changes the Negative Flag to 0
-            self.status = self.status & 0b0111_1111;
-        }
+    fn add_to_register_a(&mut self, data: u8) {
+        let sum = self.register_a as u16
+            + data as u16
+            + if self.status.get(Flag::C) { 1 } else { 0 } as u16;
+        let carry = sum > 0xff;
+        self.status.update(Flag::C, carry);
+
+        let result = sum as u8;
+        let overflow = (data ^ result) & (result ^ self.register_a) & 0x80 != 0;
+        self.status.update(Flag::V, overflow);
+
+        self.register_a = result;
+        self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    fn stack_push(&mut self, data: u8) {
+        self.mem_write((STACK_OFFSET as u16) + self.stack_pointer as u16, data);
+        self.stack_pointer = self.stack_pointer.wrapping_sub(1);
+    }
+
+    fn stack_pop(&mut self) -> u8 {
+        self.stack_pointer = self.stack_pointer.wrapping_add(1);
+        self.mem_read((STACK_OFFSET as u16) + self.stack_pointer as u16)
+    }
+
+    fn stack_push_u16(&mut self, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xFF) as u8;
+        self.stack_push(hi);
+        self.stack_push(lo);
+    }
+
+    fn stack_pop_u16(&mut self) -> u16 {
+        let lo = self.stack_pop() as u16;
+        let hi = self.stack_pop() as u16;
+
+        hi << 8 | lo
+    }
+
+    fn update_zero_and_negative_flags(&mut self, result: u8) {
+        self.status.update(Flag::Z, result == 0);
+        self.status.match_bit(Flag::N, result);
     }
 
     pub fn run(&mut self) {
-        // The CPU works in a constant cycle:
-
-        // Fetch next execution instruction from the instruction memory
-        // Decode the instruction
-        // Execute the Instruction
-        // Repeat the cycle
-
+        let ref opcodes: HashMap<u8, &'static opcodes::OpCode> = *opcodes::OPCODES_MAP;
         loop {
             // Read from the ROM the byte at the address in the PC
-            let opscode = self.mem_read(self.program_counter);
+            let code = self.mem_read(self.program_counter);
             // Increment the PC to the next instruction
             self.program_counter += 1;
+            let program_counter_state = self.program_counter;
 
-            match opscode {
-                0xA9 => {
-                    let param = self.mem_read(self.program_counter);
-                    self.program_counter += 1;
-                    self.lda(param);
-                }
-                0xAA => self.tax(),
-                0xE8 => self.inx(),
-                0x00 => return,
+            let opcode = opcodes
+                .get(&code)
+                .expect(&format!("OpCode {:x} is not recognized", code));
+
+            match opcode.mnemonic {
+                "LDA" => self.lda(&opcode.mode),
+                "LDX" => self.ldx(&opcode.mode),
+                "LDY" => self.ldy(&opcode.mode),
+                "ADC" => self.adc(&opcode.mode),
+                "TAX" => self.tax(),
+                "INX" => self.inx(),
+                "BRK" => return,
                 _ => todo!(),
+            }
+
+            if program_counter_state == self.program_counter {
+                self.program_counter += (opcode.len - 1) as u16;
             }
         }
     }
